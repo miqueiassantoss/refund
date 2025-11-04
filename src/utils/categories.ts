d84ed6c3aa506769d93c10d7706db90212d89d49
miqueiassantoss <email>
2025-11-04 16:54:05 -0300

    refund component

---
+++ src/utils/categories.ts
@@ -0,0 +1,34 @@
+import foodSvg from "../assets/food.svg"
+import othersSvg from "../assets/others.svg"
+import servicesSvg from "../assets/services.svg"
+import transportSvg from "../assets/transport.svg"
+import accommodationSvg from "../assets/accommodation.svg"
+
+export const CATEGORIES = {
+  food: {
+    name: "Alimentação",
+    icone: foodSvg
+  },
+  
+  others: {
+    name: "Outros",
+    icone: othersSvg,
+  },
+
+  services: {
+    name: "Serviços",
+    icone: servicesSvg,
+  },
+
+  transport: {
+    name: "Transporte",
+    icone: transportSvg,
+  },
+
+  accomodationSvg: {
+    name: "Acomodação",
+    icone: accommodationSvg
+  }
+}
+
+export const CATEGORIES_KEYS = Object.keys(CATEGORIES) as Array <keyof typeof CATEGORIES>
