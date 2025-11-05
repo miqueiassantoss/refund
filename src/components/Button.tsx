571caadabb4716648a605419ab097468ea49b47e
miqueiassantoss <email>
2025-11-05 11:31:38 -0300

    creating dashboard

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,24 @@
+import { classMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean
+  variant?: "base" | "icon" | "iconSmall"
+}
+
+const variants = {
+  button: {
+    base: "h-12",
+    icon: "h-12 w-12",
+    iconSmall: "h-10 w-10",
+  },
 }
 
 export function Button({
   children,
   isLoading,
+  className,
   type = "button",
+  variant = "base",
   ...rest
 }: Props) {
   return (
@@ -13,7 +26,11 @@ export function Button({
       type={type}
       {...rest}
       disabled={isLoading}
-      className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-in-out disabled:opacity-50 disabled:cursor-progress h-12"
+      className={classMerge([
+        "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-in-out disabled:opacity-50 disabled:cursor-progress",
+        variants.button[variant],
+        className,
+      ])}
     >
       {children}
     </button>
---
+++ src/routes/ManagerRoutes.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router";
+import { Dashboard } from "../pages/Dashboard";
+import { AppLayout } from "../components/AppLayout";
+import { NotFound } from "../pages/NotFound";
+
+
+export function ManagerRoutes(){
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
